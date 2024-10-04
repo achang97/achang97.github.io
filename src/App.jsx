e4f8fc2b303898d1da856b70fd7360c78b268699
Andrew Chang <andrew@acalabs.xyz>
2024-10-04 15:52:19 -0700

    scaffold vite app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My GitHub Pages App</h1>
+        <p>This is a React app deployed on GitHub Pages using Vite.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
