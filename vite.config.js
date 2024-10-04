185018a7d05f56de2d196b07c1ae773c97dddac5
Andrew Chang <andrew@acalabs.xyz>
2024-10-04 16:10:00 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/achang97.github.io/', // Replace with your repository name
+  base: '/', // Replace with your repository name
 });
